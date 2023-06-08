7bcea15bb6e4645da144f06425339c58c3750ad4
mudit247365 <guptmudit2005@gmail.com>
2023-06-08 17:22:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React, { Component } from 'react'
-import NavBar from './Components/NavBar'
-import News from './Components/News'
+import React, { Component } from 'react';
+import NavBar from './Components/NavBar';
+import News from './Components/News';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
-  render() {
+  pagesize=6;
+  state={
+    progress:0
+  }
+  setProgress=(progress)=>{
+    this.setState({progress:progress})
+  }
+  render() {    
     return (
+
       <div>
-        <NavBar/>
-        <News/>
-        
+        <Router>
+          <NavBar />
+          <LoadingBar
+        color='#f11946'
+        height={8}
+        progress={this.state.progress}       
+      />
+          <Routes>
+            <Route path="/"              element={<News setProgree={this.setProgress}  key ="general" country="in" pageSize={this.pagesize} category="general" />} />
+            <Route path="/Entertainment"  element={<News setProgree={this.setProgress} key ="entertainment" country="in" pageSize={this.pagesize} category="entertainment" />} />
+            <Route path="/Health"         element={<News setProgree={this.setProgress} key ="health" country="in" pageSize={this.pagesize} category="health" />} />
+            <Route path="/Science"        element={<News setProgree={this.setProgress} key ="Science" country="in" pageSize={this.pagesize} category="Science" />} />
+            <Route path="/Business"       element={<News setProgree={this.setProgress} key ="business" country="in" pageSize={this.pagesize} category="business" />} />
+            <Route path="/Sports"         element={<News setProgree={this.setProgress} key ="sports" country="in" pageSize={this.pagesize} category="sports" />} />
+          </Routes>
+        </Router>
       </div>
-    )
+    );
   }
 }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -5,18 +5,24 @@ export default class NewsItem extends Component {
   
 
   render() {
-    let {title,description,url,id}=this.props;
+    let {title,description,url,id,author,date}=this.props;
     return (
-      <div>
-        <div className="card" style={{width: "18rem"}}>
-  <img src={url} className="card-img-top" alt="..."/>
+  <div>
+  <div className="card" >
+    <div style={{display:'flex',justifyContent:'center',position:'absolute',right:'0'}}>
+  <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" >
+       {"source"}
+  </span>  
+  </div>
+  <img src={url?url:''} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href={id} target="_blank" className="btn btn-sm btn-primary">Read more</a>
+    <h5 className="card-title" >{title?title:"My NEWS"} </h5>
+    <p className="card-text">{description?description:"news is a very important part of our life"}</p>
+    <p className="card-text"><small className="text-muted">By {author?author:"Unknown"} on {new Date(date).toGMTString() }</small></p>
+    <a href={id} target="blank"  className="btn btn-sm btn-dark" >Read more</a>
   </div>
 </div>
-      </div>
+</div>
     )
   }
 }
